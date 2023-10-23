e6ca296d43c266ae719720b208c550e037c995ce
shweta dand <dandshweta2017@gmail.com>
2023-10-23 22:50:02 +0530

    Dynamic Form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
+import Form from "./components/Form";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Form Builder</h1>
+      {/* <h1>Form Builder</h1> */}
+
+      <Form />
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import "../assets/Styles/Form.css";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    questions: [
+      {
+        questionText: "",
+        answer: "",
+      },
+    ],
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleAddQuestion = () => {
+    const newQuestion = {
+      questionText: "",
+      answer: "",
+    };
+
+    setFormData({
+      ...formData,
+      questions: [...formData.questions, newQuestion],
+    });
+  };
+
+  const handleQuestionChange = (index, e) => {
+    const updatedQuestions = [...formData.questions];
+    const { name, value } = e.target;
+    updatedQuestions[index][name] = value;
+    setFormData({ ...formData, questions: updatedQuestions });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Untitled Form"
+          value={formData.title}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <input
+          id="description"
+          name="description"
+          placeholder="Form description"
+          value={formData.description}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        {formData.questions.map((question, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              name="questionText"
+              placeholder="Question"
+              value={question.questionText}
+              onChange={(e) => handleQuestionChange(index, e)}
+            />
+            <input
+              type="text"
+              name="answer"
+              placeholder="Answer"
+              value={question.answer}
+              onChange={(e) => handleQuestionChange(index, e)}
+            />
+          </div>
+        ))}
+        <button type="button" onClick={handleAddQuestion}>
+          Add Question
+        </button>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
